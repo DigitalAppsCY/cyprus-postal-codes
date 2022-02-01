0d39b25b93ae86a2d136ad5774430d56f0133272
Despo Pentara <despo@extractmethod.com>
2022-02-01 23:10:23 +0000

    get calls the requested resource with the specified parameters

--- spec/cyprus_postal_codes/client_spec.rb
+++ spec/cyprus_postal_codes/client_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe CyprusPostalCodes::Client do
       expect { described_class.new }.to raise_error(ArgumentError, "missing keyword: :api_key")
     end
   end
+
+  describe "get" do
+    let(:connection) { instance_double(Faraday::Connection, get: Struct.new(:body).new({ data: "" })) }
+    let(:service) { instance_double(described_class) }
+    let(:params) { { post_code: 8020 } }
+
+    it "calls the requested resource with the specified parameters" do
+      allow(Faraday).to receive(:new).and_return(connection)
+      allow(described_class).to receive(:initialize).and_return(service)
+
+      described_class.new(api_key: "api_key").get("address", params)
+
+      expect(connection).to have_received(:get).with("address", params)
+    end
+  end
 end
